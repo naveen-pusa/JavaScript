513a7921d64dbc32f867549d87381f12056516bd
Naveen-pusa <naveenpusa2000@gmail.com>
2026-02-24 14:05:52 +0530

    Events

--- DomManipulations/app.js
+++ DomManipulations/app.js
@@ -7,11 +7,10 @@
 // insertBefore(newList ,li2)
 // replaceChild
 
-// innerHtml textContent innerText
 
 // classList,className
 
-// let cont =  document.getElementById("container")
+let cont =  document.getElementById("container")
 // console.log(cont);
 
 // createElement
@@ -25,28 +24,65 @@
 
 // }
 
-let mobiles = document.getElementById('mobiles')
+// let mobiles = document.getElementById('mobiles')
 
-let list1 = document.getElementById('list1') 
-let list2 = document.getElementById('list2') 
-let list3 = document.getElementById('list3') 
-let list4 = document.getElementById('list4') 
-let list5 = document.getElementById('list5')
+// let list1 = document.getElementById('list1') 
+// let list2 = document.getElementById('list2') 
+// let list3 = document.getElementById('list3') 
+// let list4 = document.getElementById('list4') 
+// let list5 = document.getElementById('list5')
 
-let list6 = document.createElement('li')
-list6.setAttribute('id','list6')
-list6.style.color = 'red'
-list6.innerText = 'Apple'
+// let list6 = document.createElement('li')
+// list6.setAttribute('id','list6')
+// list6.style.color = 'red'
+// list6.innerText = 'Apple'
 
-let list7 = document.createElement('li')
-list7.setAttribute('id','list7')
-list7.innerText = 'microsoft'
-list7.style.color='maroon'
+// let list7 = document.createElement('li')
+// list7.setAttribute('id','list7')
+// list7.innerText = 'microsoft'
+// list7.style.color='maroon'
 
 
-mobiles.insertBefore(list6,list4)
+// mobiles.insertBefore(list6,list4)
 
-mobiles.replaceChild(list7,list3)
+// mobiles.replaceChild(list7,list3)
+
+// innerHtml ,textContent, innerText
+
+
+// document.body.innerHTML= '<h1>NamasteJavascript</h1>';
+// document.body.textContent= '<h1>NamasteJavascript</h1>';
+// document.body.innerText= '<h1>NamasteJavascript</h1>';
+
+
+// let a = h1.innerText
+// alert(a)
+// let b = h1.textContent
+// alert(b)
+
+// ClassName , classList 
+
+// className
+
+
+let h1 = document.getElementById('h1')
+h1.className= 'text'
+// h1.className ='color'
+
+// classList
+    // add
+    // remove
+    // replace
+    // toggle
+    // contains
+
+// h1.classList.add('text' ,'color')
+// h1.classList.add('text')
+// h1.classList.replace('text','color')
+
+
+
+
 
 
 
---
+++ Events/app.js
@@ -0,0 +1,17 @@
+// Events
+   
+// pointerEvents
+    // onCLick
+    // ondbleClick
+// mouseEvents
+// keyboardEvents
+// focusEvents
+// formEvents
+
+
+function trigger(event) {
+    document.querySelector('h1').innerHTML = 'welcome to the page'
+    // console.log(event);
+    
+    // alert('This is Event DlbClick')
+}
