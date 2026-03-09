7da3ca28275b7aa6813d90fa5af83c52798aae03
Naveen-pusa <naveenpusa2000@gmail.com>
2026-03-09 19:22:14 +0530

    map & filter syntax

--- arrayiterations/app.js
+++ arrayiterations/app.js
@@ -1,3 +1,9 @@
+// function test(){
+//    return "Hello"
+   
+// }
+// console.log(test());
+
 // ArrayIteration
 
 
@@ -25,16 +31,64 @@ let mobilep = [
 //     </div>
  
 
-let container =  document.getElementById('container')
-mobilep.forEach(product=>{
-    container.innerHTML += `
-      <div id="card">
-         <h1>${product.title}</h1>
-         <img src="${product.img}" alt="">
-         <p>${product.desc}</p>
-     </div>  
+// ForEach:
+
+// let container =  document.getElementById('container')
+// mobilep.forEach(product=>{
+//     container.innerHTML += `
+//       <div id="card">
+//          <h1>${product.title}</h1>
+//          <img src="${product.img}" alt="">
+//          <p>${product.desc}</p>
+//      </div>  
       
-    `
+//     `
+// })
+
+
+// map:
+
+
+let products =[
+
+{tittle:'iphone', img:'https://media.idownloadblog.com/wp-content/uploads/2018/07/Apple-logo-black-and-white.png',           cost:100000, about:'This is iphone'  },
+{tittle:'vivo'  , img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRotV2QgOFVYi3-6KE02eVVesLjOEcT5jtmHw&s',        cost:50000 ,  about:'This is Vivo'    },    
+{tittle:'oppo'  , img:'https://5.imimg.com/data5/CY/EC/MY-33491000/oppo-logo-board-500x500.jpg',                             cost:30000 ,  about:'This is Oppo'    },
+{tittle:'pixel' , img:'https://www.shutterstock.com/image-vector/pixel-logo-technology-600w-487093207.jpg',                  cost:80000 ,  about:'This is Pixel'   },    
+{tittle:'lava'  , img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEEfwCIZ7RQHYHEMLzCV7_usyIEOw4FnsazQ&s',        cost:20000 ,  about:'This is Lava'    },
+{tittle:'redmi' , img:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Xiaomi_logo.svg/960px-Xiaomi_logo.svg.png', cost:25000 ,  about:'This is redmi'   },    
+]
+
+
+
+let container = document.getElementById('container')
+ 
+
+let phones = products.filter(mobile =>(mobile.cost)<50000)
+.map(mobile=>{
+    return  `
+    <div id="card">
+    <h2>${mobile.tittle}</h2>
+    <img src="${mobile.img}" height="200px" alt="">
+    <mark>${mobile.cost}</mark>
+    <p>${mobile.about}</p>
+</div>
+   `
+  
+    
 })
+container.innerHTML=phones.join('')
+// console.log(phones);
+
+products
+.filter(mobile =>(mobile.cost )< 50000)
+.map(mobile=> mobile.tittle)
+.forEach(tittle=> console.log(tittle))
+
+// console.log(products[0].mobile);
+// console.log(products[1].mobile);
+// console.log(products[2].mobile);
+
+
 
 
