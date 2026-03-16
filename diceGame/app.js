9945ac545a56237e5e779f3c31e514be87a84b24
Naveen-pusa <naveenpusa2000@gmail.com>
2026-03-16 17:17:34 +0530

    timing_function

--- diceGame/app.js
+++ diceGame/app.js
@@ -1,15 +1,15 @@
 
 
 function diceGame() {
-    let dice1 = document.getElementById('dice1')
+    let dice = document.getElementById('dice')
     let h2  = document.getElementById('h2')
    
     h2.innerHTML = ''
-     dice1.classList.add('addRotate')
+     dice.classList.add('addRotate')
     setTimeout(()=>{
          let randomNum = Math.floor(Math.random()*7)
         h2.innerHTML = randomNum
-        dice1.classList.remove('addRotate')
+        dice.classList.remove('addRotate')
     },2000)
 
 
